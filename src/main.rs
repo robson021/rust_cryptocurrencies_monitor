8d263c5816db9ff4d19e8e94a5622594a75d54d5
robson021 <nowak021@gmail.com>
2024-11-30 18:17:00 +0100

    add http get and cargo dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use serde::Deserialize;
+use std::error::Error;
+
+#[derive(Deserialize)]
+struct ApiResponse {
+    id: String,
+    name: String,
+    symbol: String,
+    rank: String,
+    price_usd: String, //todo: use decimal type
+}
+
+fn http_get(api_url: &str) -> Result<String, Box<dyn Error>> {
+    println!("Fetching assets from: '{}'.", api_url);
+    let resp = reqwest::blocking::get(api_url)?.text()?;
+    println!("Received response:\n{:#?}", resp);
+    Ok(resp)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let api_url = "https://api.coincap.io/v2/assets/";
+    let result = http_get(api_url).unwrap();
+
+    println!("Result:\n{}", result);
 }
